685e9a899e82ef678af6263ade0c49aab9f56ae0
JavierGMo <jinjuriki62@gmail.com>
2020-10-19 03:08:04 -0500

    OAuthUpdate

--- server/mailer/mailer.js
+++ server/mailer/mailer.js
@@ -1,4 +1,9 @@
 const nodeMailer = require('nodemailer');
+const {google} = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+
+
+
 
 
 module.exports = async function(to, subject, text, fullName){
@@ -12,6 +17,21 @@ module.exports = async function(to, subject, text, fullName){
             </ul>
             <p>Descripcion del mail: ${text}</p>
         `;
+
+        const oauth2Client = new OAuth2(
+            process.env.CID,
+            process.env.CSECR,
+            'https://developers.google.com/oauthplayground'
+        );
+        
+
+        oauth2Client.setCredentials({
+            refresh_token : process.env.REFTKN
+        });
+        
+        //Cuando expire el token esta linea nos va a generar una nueva
+        const accessToken = oauth2Client.getAccessToken();
+
         const transport = nodeMailer.createTransport({
             host : 'smtp.gmail.com',
             port : 465,
@@ -21,7 +41,10 @@ module.exports = async function(to, subject, text, fullName){
                 user : process.env.MAL,
                 // pass : process.env.ACC
                 clientId : process.env.CID,
-                clientSecret : process.env.CSECR
+                clientSecret : process.env.CSECR,
+                refreshToken : process.env.REFTKN,
+                accessToken : accessToken
+
             }
         });
     
