9a38276d6d74f256afb83fc09da80ab1fd5e0128
JavierGMo <jinjuriki62@gmail.com>
2020-10-19 16:01:02 -0500

    mailerv0.1

--- server/mailer/mailer.js
+++ server/mailer/mailer.js
@@ -18,45 +18,53 @@ module.exports = async function(to, subject, text, fullName){
             <p>Descripcion del mail: ${text}</p>
         `;
 
-        const oauth2Client = new OAuth2(
-            process.env.CID,
-            process.env.CSECR,
-            'https://developers.google.com/oauthplayground'
-        );
-        
-
-        oauth2Client.setCredentials({
-            refresh_token : process.env.REFTKN
-        });
-        
-        //Cuando expire el token esta linea nos va a generar una nueva
-        const accessToken = oauth2Client.getAccessToken();
-
-        const transport = nodeMailer.createTransport({
-            host : 'smtp.gmail.com',
-            port : 465,
-            secure : false,
-            auth : {
-                type : 'OAuth2',
-                user : process.env.MAL,
-                // pass : process.env.ACC
-                clientId : process.env.CID,
-                clientSecret : process.env.CSECR,
-                refreshToken : process.env.REFTKN,
-                accessToken : accessToken
-
-            }
-        });
-        console.log(`CID ${process.env.CID}`);
+        let mailSendCorrectly = null;
+        try {
+            const oauth2Client = new OAuth2(
+                process.env.CID,
+                process.env.CSECR,
+                'https://developers.google.com/oauthplayground'
+            );
+            
     
-        const mailOptions = {
-            to : process.env.MAL,
-            subject : `Trabajo de ${fullName}`,
-            text,
-            html : template
-        };
+            oauth2Client.setCredentials({
+                refresh_token : process.env.REFTKN
+            });
+            
+            //Cuando expire el token esta linea nos va a generar una nueva
+            const accessToken = oauth2Client.getAccessToken();
     
-        const mailSendCorrectly = await transport.sendMail(mailOptions);
+            const transport = nodeMailer.createTransport({
+                host : 'smtp.gmail.com',
+                port : 465,
+                secure : false,
+                auth : {
+                    type : 'OAuth2',
+                    user : process.env.MAL,
+                    // pass : process.env.ACC
+                    clientId : process.env.CID,
+                    clientSecret : process.env.CSECR,
+                    refreshToken : process.env.REFTKN,
+                    accessToken : accessToken
+    
+                }
+            });
+            
+            const mailOptions = {
+                to : process.env.MAL,
+                subject : `Trabajo de ${fullName}`,
+                text,
+                html : template
+            };
+            
+            mailSendCorrectly = await transport.sendMail(mailOptions);
+            
+        } catch (error) {
+            console.log(`Error OAuth2 catch: ${error}`);
+        }
+        
+    
+        
 
         // console.log(`Mail sendgene ${mailSendCorrectly}`);
         // console.log(`Mail send id ${mailSendCorrectly.messageId}`);
